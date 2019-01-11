e230caeeee5bbb8a79053c875102e6d63e267d33
androids7 <1559215761@qq.com>
2019-01-11 21:31:04 +0800

    修复不能同名文件zip读取

--- LavaRuntime/src/as/mke/lavrun/ReadFile.java
+++ LavaRuntime/src/as/mke/lavrun/ReadFile.java
@@ -5,10 +5,14 @@ import java.nio.charset.Charset;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
+import java.util.*;
 
 public class ReadFile {
     public static InputStream readFile(String path,String filename) throws IOException {
        // String path = "F:\\*******\\201707\\78641695079026649.zip";
+	   
+	   
+	   /*
         ZipFile zf = new ZipFile(path);
 		InputStream ine=null;
         InputStream in = new BufferedInputStream(new FileInputStream(path));
@@ -22,21 +26,54 @@ public class ReadFile {
 		if(ze.getName().equals(filename)){
             //    BufferedReader br = new BufferedReader(
 				ine=zf.getInputStream(ze);
-					/*
-                String line;
-                while((line = br.readLine()) != null){
-                    System.out.println(line.toString());
-                }
-              */
+				
+              
             }
             
 			
 			}
      //  zin.closeEntry();
-    
+    */
 	
+	
+		InputStream ine=null;
+	
+		Charset gbk = Charset.forName("utf8");
+        ZipFile zip = new ZipFile(new File(path), gbk);
+        for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            String zipEntryName = entry.getName();
+			
+			if(zipEntryName.equals(filename)){
+			
+            ine = zip.getInputStream(entry);
+			}
+			/*
+            String outPath = (descDir + zipEntryName).replaceAll("\\*", "/");
+            // 判断路径是否存在,不存在则创建文件路径
+            File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            // 判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压
+            if (new File(outPath).isDirectory()) {
+                continue;
+            }
+            // 输出文件路径信息
+            System.out.println(outPath);
+
+            OutputStream out = new FileOutputStream(outPath);
+            byte[] buf1 = new byte[1024];
+            int len;
+            while ((len = in.read(buf1)) > 0) {
+                out.write(buf1, 0, len);
+            }
+            in.close();
+            out.close();
+	*/
+	
+	}
 	return ine;
 	
 	}
-	
-}
+	}
